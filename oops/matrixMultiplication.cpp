44c17ccd0c2ee489e2282a4c8f8819596eba210a
syedareehaquasar <reeha.saba@gmail.com>
2021-03-23 15:12:47 +0530

    need to ask - doubt

--- oops/matrixMultiplication.cpp
+++ oops/matrixMultiplication.cpp
@@ -1,1 +1,69 @@
+#include <iostream>
 
+using namespace std;
+
+class MatrixMultiplication{
+    public:
+        int n, m, p, q;
+
+        void InputMatrix();
+        void multiply(int m, int n, int p, int q, int a[m][n], int b[p][q]);
+        void result(int m, int q, int c[m][q]);
+};
+
+void MatrixMultiplication::InputMatrix(){
+    cout << "Enter no. of rows and columns of first Matrix" << endl;
+    cin >> m >> n;
+    cout << "Enter no. of rows and columns of second Matrix" << endl;
+    cin >> p >> q;
+    if (n == p) {
+        int a[m][n];
+        int b[p][q];
+        cout << "Enter the values for the first matrix row wise" << endl;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++)
+            {
+                cin >> a[i][j];
+            }
+        }
+        cout << "Enter the values for the second matrix row wise" << endl;
+        for (int i = 0; i < p; i++) {
+            for (int j = 0; j < q; j++) {
+                cin >> b[i][j];
+            }
+        }
+        multiply(m, n, p, q, a, b);
+    }
+    else {
+        cout << "Can't multiply matrices - matrices are not confortable for multiplication" << endl;
+    }
+}
+
+void MatrixMultiplication::multiply(int m, int n, int p, int q, int a[m][n], int b[p][q]){
+    int c[m][q];
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < q; j++) {
+            c[i][j]=0;
+            for (int k = 0; k < n; k++) {
+                c[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+    result(m, q, c);
+}
+
+void MatrixMultiplication::result(int m, int q, int c[m][q]){
+    cout << "The Resultant Matrix is: \n";
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < q; j++) {
+            cout << c[i][j];
+        }
+        cout << endl;
+    }
+}
+
+int main(){
+    MatrixMultiplication x;
+    x.InputMatrix();
+    return 0;
+}
--- oops/studentRecord.cpp
+++ oops/studentRecord.cpp
@@ -1,1 +1,73 @@
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+class studentRecord{
+    private:
+        string name;
+        string branch;
+        int rollNo;
+        int age;
+        char sex[15];
+        float marks;
+
+    public:
+        void getDetails();
+        void check();
+        void showDetails();
+        void details();
+};
+
+void studentRecord::getDetails(){
+    cout << "Enter name" << endl;
+    cin >> name;
+    cout << "Enter branch" << endl;
+    cin >> branch;
+    cout << "Enter Roll no. "<< endl;
+    cin >> rollNo;
+    cout << "Enter age" << endl;
+    cin >> age;
+    cout << "Enter sexuality" << endl;
+    cin >> sex;
+    cout << "Enter total marks scored in 5 subjects" << endl;
+    cin >> marks;
+}
+
+void studentRecord::showDetails(){
+    cout << "Name: " << name << endl;
+    cout << "Percentage: " <<  (marks/5) << "%" << endl;
+}
+
+void studentRecord::check(){
+    if (((marks/5)) > 70) {
+        showDetails();
+    }
+}
+
+void studentRecord::details(){
+    cout << "name: " << name << ", branch: " << branch << ", roll no.: " << rollNo << ", age: " << age << ", sex: " << sex << ", marks: " << marks << endl;
+}
+
+int main(int argc, char const *argv[]){
+    int students;
+    cout << "Enter the no. of students: ";
+    cin >> students;
+    studentRecord studentsArr[students];
+    for (int i = 0; i < students; ++i) {
+        cout << "For student" << i + 1 << " :" << endl;
+        studentsArr[i].getDetails();
+    }
+
+    cout << "\nYou have entered:" << endl;
+    for (int i = 0; i < students; i++) {
+        studentsArr[i].details();
+    }
+
+    cout << "\n\n\n" << endl;
+
+    cout << "Students having marks greater than 70% are: " << endl;
+    for (int i = 0; i < students; i++) {
+        studentsArr[i].check();
+    }
+}
